5df7be772b4438ff288cae7d3013404f8a00fd5a
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-03-13 11:37:17 -0300

    API algaworks - edlog criada

---
+++ edlogSpring/edlog-api/src/test/java/com/edvaldoleite/edlog/EdlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edvaldoleite.edlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
